55b0dc233cfccafa25284b90ca9cfe91ed656bc8
Daniel Burgner <dburgnerjr@yahoo.com>
2019-11-25 13:13:35 -0600

    Phase 1 Subtask 1.
    Completed first subtask for creating a new web application.

---
+++ WebContent/index.jsp
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Cindy's Resume Appointment Scheduling Service</title>
+<style type="text/css">
+body {
+	background-image: url('https://cdn.crunchify.com/bg.png');
+}
+</style>
+</head>
+<body>
+	<br>
+	<div style="text-align: center">
+		<h2>
+			Cindy's Resume Appointment Scheduling Service<br> <br>
+		</h2>
+		<h3>
+			<a href="appointment_scheduler.html"><img src="images/cindys_resume.jfif" alt="Cindy's Resume" width="225" height="225"></a><br>
+			Please click on the image to continue.
+		</h3>
+	</div>
+</body>
+</html>
---
+++ src/com/cindysresumeservice/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.cindysresumeservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String getIndexPage() {
+//		return "IndexPage";
+//	}
+
+	@RequestMapping(value = "/appointment_scheduler", method = RequestMethod.GET)
+	public String getAppointmentScheduler() {
+		return "appointment_scheduler";
+	}
+
+}
